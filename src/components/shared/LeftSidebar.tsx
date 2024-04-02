4f0753cb7981d9fdbde0aba3e5d7bda14fc41555
riyajath-ahamed <riyajatha@gmail.com>
2024-04-02 22:59:52 +0530

    WIP Leftside bar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+
 function LeftSidebar() {
-    return <h1>LeftSidebar</h1>
+    return (
+        <section className="custom-scrollbar leftsidebar">
+            <div className="flex w-full flex-1 flex-col gap-6 px-6">
+                <Link
+                href={`/`}
+                >
+                </Link>
+                <p>Fin</p>
+
+            </div>
+
+        </section>
+    )
 }
 
 export default LeftSidebar;
